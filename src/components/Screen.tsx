bea0790de73f605bed22c65e1f62454c35dc7e6e
02danieljonas <02danieljonas@gmail.com>
2024-02-20 06:56:47 -0500

    Added flex to the Screen and its children

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,5 +1,10 @@
 const Screen = () => {
-    return <div className="bg-background w-screen h-screen">Screen</div>;
+    return (
+        <div className="bg-background w-screen h-screen flex flex-col">
+            <div className="w-full flex-auto">hi</div>
+            <div className="h-14 bg-opacity-65 bg-cyan-500 w-full">hello</div>
+        </div>
+    );
 };
 
 export default Screen;
