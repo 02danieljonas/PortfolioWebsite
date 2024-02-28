278772b49a5b3e5231e4f8f454bb7605c38f19ed
02danieljonas <02danieljonas@gmail.com>
2024-02-28 08:54:57 -0500

    Changed text color

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Screen from "./components/Screen";
 
 function App() {
-    return <div className="bg-black text-white"><Screen/></div>;
+    return (
+        <div>
+            <Screen />
+        </div>
+    );
 }
 
 export default App;
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ const Apps = ({ name }: AppsProps) => {
     return (
         <div className="relative pb-8">
             <div className="w-12 h-12 bg-white p-2 text-black "></div>
-            <div className="absolute overflow-hidden w-24 text-center left-6 -translate-x-1/2 bg">
+            <div className="absolute overflow-hidden w-24 text-center left-6 -translate-x-1/2 bg text-white">
                 {name}
             </div>
         </div>
