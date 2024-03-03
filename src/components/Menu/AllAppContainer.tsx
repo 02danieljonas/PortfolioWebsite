f3e387e27226d7f5442f472678f3043b5c83f212
02danieljonas <02danieljonas@gmail.com>
2024-03-03 10:20:04 -0500

    Move my list of apps to screen

--- src/components/Menu/AllAppContainer.tsx
+++ src/components/Menu/AllAppContainer.tsx
@@ -1,52 +1,28 @@
-import { useState } from "react";
 import AppShortcut from "../AppShortcut";
+import App from "../types/App.interface";
 
 interface AllAppContainerProps {
     search: string;
 }
 
-interface App {
-    name: string;
-    id: number;
-    tags: string[];
-    path: string;
-    img: string;
-    url: string;
-}
-
 const AllAppContainer = ({ search }: AllAppContainerProps) => {
-    const [appNames] = useState<App[]>([
-        {
-            id: 1,
-            name: "Example",
-            tags: ["menu-pinned"],
-            path: "",
-            img: "/app-icons/example.jpg",
-            url: "https://example.com/",
-        },
-        { id: 2, name: "Microsoft Word", tags: [], path: "", img: "", url: "" },
-        { id: 3, name: "Google Chrome", tags: [], path: "", img: "", url: "" },
-        { id: 4, name: "VS Code", tags: [], path: "", img: "", url: "" },
-        { id: 5, name: "File Explorer", tags: [], path: "", img: "", url: "" },
-        { id: 6, name: "Resume", tags: [], path: "", img: "", url: "" },
-        { id: 7, name: "Firefox", tags: [], path: "", img: "", url: "" },
-        { id: 8, name: "Notes", tags: [], path: "", img: "", url: "" },
-        { id: 9, name: "Calculator", tags: [], path: "", img: "", url: "" },
-        { id: 10, name: "CodeClash", tags: [], path: "", img: "", url: "" },
-        { id: 11, name: "Memory Game", tags: [], path: "", img: "", url: "" },
-        { id: 12, name: "Youtube", tags: [], path: "", img: "", url: "" },
-        { id: 13, name: "Github", tags: [], path: "", img: "", url: "" },
-        { id: 14, name: "Minecraft", tags: [], path: "", img: "", url: "" },
-    ]);
+    const appList: App[] = [];
 
-    const filteredApps = appNames.filter((app) =>
+    const filteredApps = appList.filter((app) =>
         app.name.toLowerCase().includes(search.trim().toLowerCase())
     );
 
     return (
         <div className="flex flex-wrap p-5 gap-x-8 gap-y-4">
             {filteredApps.map((e) => (
-                <AppShortcut name={e.name} key={e.id} tags={e.tags} path={e.path} img={e.img} url={e.url} />
+                <AppShortcut
+                    name={e.name}
+                    key={e.id}
+                    tags={e.tags}
+                    path={e.path}
+                    img={e.img}
+                    url={e.url}
+                />
             ))}
         </div>
     );
---
+++ src/components/types/App.interface.ts
@@ -0,0 +1,8 @@
+export default interface App {
+    name: string;
+    id: number;
+    tags: string[];
+    path: string;
+    img: string;
+    url: string;
+}
