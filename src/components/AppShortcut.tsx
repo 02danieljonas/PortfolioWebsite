ea14cf81e507230f89d1f502224a531276d7bc22
02danieljonas <02danieljonas@gmail.com>
2024-03-04 11:22:48 -0500

    Improved text styling

--- src/components/AppShortcut.tsx
+++ src/components/AppShortcut.tsx
@@ -9,13 +9,14 @@ const AppShortcut = ({ appInfo }: AppShortcutProps) => {
     const { openApp } = useAppContext();
     return (
         <div
-            className="relative pb-8 cursor-pointer"
+            className="relative pb-8 cursor-pointer w-16"
             onClick={() => openApp(appInfo.id)}
         >
-            <div className="w-12 h-12 p-2 text-black ">
+            <div className="bg-white absolute opacity-15 w-full h-full"></div>
+            <div className="w-12 h-12 p-2 text-black mx-auto">
                 <img src={appInfo.img} alt={`Image of ${appInfo.name}`} />
             </div>
-            <div className="absolute overflow-hidden w-24 text-center left-6 -translate-x-1/2 bg text-white leading-4">
+            <div className="absolute left-1/2 -translate-x-1/2 line-clamp-2 text-center mx-auto text-white leading-4">
                 {appInfo.name}
             </div>
         </div>
