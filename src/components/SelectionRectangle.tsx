a1d52a71c5b7e260b8e62fa2d26754a7bb0ac02f
02danieljonas <02danieljonas@gmail.com>
2024-03-04 09:37:12 -0500

    Changed position of functions

--- src/components/SelectionRectangle.tsx
+++ src/components/SelectionRectangle.tsx
@@ -5,6 +5,29 @@ interface Position {
     y: number;
 }
 
+interface SelectionRectangleProps {
+    startPos: Position;
+    endPos: Position;
+}
+
+const SelectionRectangle = ({ startPos, endPos }: SelectionRectangleProps) => {
+    const smallX = Math.min(startPos.x, endPos.x);
+    const smallY = Math.min(startPos.y, endPos.y);
+    const bigX = Math.max(startPos.x, endPos.x);
+    const bigY = Math.max(startPos.y, endPos.y);
+    return (
+        <div
+            className="fixed bg-cyan-500 opacity-40 border-2 border-blue-950 pointer-events-none"
+            style={{
+                top: smallY,
+                left: smallX,
+                width: bigX - smallX,
+                height: bigY - smallY,
+            }}
+        ></div>
+    );
+};
+
 const SelectionRectangleContainer = () => {
     const [startPos, setStartPos] = useState<Position>({ x: 0, y: 0 });
     const [isSelectionActive, setIsSelectionActive] = useState<boolean>(true);
@@ -36,29 +59,6 @@ const SelectionRectangleContainer = () => {
                 <SelectionRectangle startPos={startPos} endPos={endPos} />
             )}
         </div>
-    );
-};
-
-interface SelectionRectangleProps {
-    startPos: Position;
-    endPos: Position;
-}
-
-const SelectionRectangle = ({ startPos, endPos }: SelectionRectangleProps) => {
-    const smallX = Math.min(startPos.x, endPos.x);
-    const smallY = Math.min(startPos.y, endPos.y);
-    const bigX = Math.max(startPos.x, endPos.x);
-    const bigY = Math.max(startPos.y, endPos.y);
-    return (
-        <div
-            className="fixed bg-cyan-500 opacity-40 border-2 border-blue-950 pointer-events-none"
-            style={{
-                top: smallY,
-                left: smallX,
-                width: bigX - smallX,
-                height: bigY - smallY,
-            }}
-        ></div>
     );
 };
 
