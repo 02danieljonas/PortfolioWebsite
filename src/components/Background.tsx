a6f9f4d16e0f00775c717767f53ce1dd9e7d207e
02danieljonas <02danieljonas@gmail.com>
2024-03-09 16:35:00 -0500

    Removed onContextMenu

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,10 +2,6 @@ const Background = () => {
     return (
         <div
             className={`absolute bg-background1 w-screen h-screen bg-cover `}
-            onContextMenu={(e) => {
-                e.preventDefault(); // prevent the default behaviour when right clicked
-                console.log("Right Click");
-            }}
             onDoubleClick={() => {
                 console.log("hello");
             }}
